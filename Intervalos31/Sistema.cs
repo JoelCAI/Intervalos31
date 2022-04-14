5994a1a9b489b7222a8c15e46ca2cbb302b624ac
JoelCAI <joelargentina12345@gmail.com>
2022-04-14 16:55:36 -0300

    Agregar archivos de proyecto.

---
+++ Intervalos31/Sistema.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos31
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+            Console.WriteLine("\n Recuerde seguir las instrucciones. Presione cualquier tecla para continuar");
+            
+
+            string fecha = Validador.ValidarString("\n Para saber si lo ingresado es un intervalo de tiempo ingrese" +
+                                                   " los días y horas en este formato." +
+                                                   "\n *dddd:hh:mm:ss* ");
+
+         
+
+            bool timeSpanCorrecto = TimeSpan.TryParse(fecha, out TimeSpan fechaIngresada);
+
+            if (timeSpanCorrecto)
+            {
+                Console.Clear();
+                Console.WriteLine("\n La fecha ingresada SI es un intervalo de tiempo: \n" 
+                                  + " " + fechaIngresada.Days + " días, "
+                                  + fechaIngresada.Hours + " horas, "
+                                  + fechaIngresada.Minutes + " minutos, "
+                                  + fechaIngresada.Seconds + " segundos. ");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("\n La fecha ingresada NO es un intervalo de tiempo" +
+                                  "\n Usted no ingreso este formato *dddd:hh:mm:ss* ");
+            }
+           
+
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ Intervalos31/Validador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos31
+{
+    public class Validador
+    {
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+
+        public static string ValidarString(string mensaje)
+        {
+
+            string opcion;
+            bool valido = false;
+            string mensajeValidador = "";
+            string mensajeError = "\n Por favor ingrese un valor no vacio para que pueda continuar. ";
+
+            do
+            {
+
+                Console.WriteLine(mensaje);
+                Console.WriteLine(mensajeValidador);
+
+                opcion = Console.ReadLine();
+
+                if (opcion == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n");
+                    Console.WriteLine(mensajeError);
+                }
+                else
+                {
+                    valido = true;
+                }
+
+            } while (!valido);
+
+            return opcion;
+        }
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+    }
+}
